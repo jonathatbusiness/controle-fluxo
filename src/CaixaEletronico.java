eb88a778bcab9af50f130d178dcbcf083d97aa1e
Jonatha L Teixeira <106690382+jonathatbusiness@users.noreply.github.com>
2022-10-16 12:50:42 -0300

    Data Changes

--- src/CaixaEletronico.java
+++ src/CaixaEletronico.java
@@ -1,3 +1,18 @@
 public class CaixaEletronico {
+    public static void main(String[] args) {
+        double saldo = 25.0;
+        double valorSolicitado = 26.0;
 
+        if (valorSolicitado < saldo) {
+            saldo = saldo - valorSolicitado;
+            System.out.println("Saldo sacado: " + valorSolicitado);
+            System.out.println("Novo Saldo: " + saldo);
+
+        } else {
+            System.out.println("Valor solicitado: " + valorSolicitado);
+            System.out.println("Valor disponível: " + saldo);
+            System.out.println("Saldo insuficiente");
+        }
+
+    }
 }
